7196a260375136b1b44f0fe402c8d410372c01fd
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-23 10:51:09 +0900

    Home.vueのpathを通した。
    alias '@'を適用。
    vite.config.ts, tsconfig.jsonに記載した。
    [Vite + React で path alias を使いたい！ - かもメモ](https://chaika.hatenablog.com/entry/2022/05/14/083000)
    [共通オプション | Vite](https://ja.vitejs.dev/config/shared-options.html#resolve-alias)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from '@/views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {path: '/', name: 'Home', component: Home}
+  ],
+})
 
 createApp(App).mount('#app')
