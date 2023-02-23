96b9df09cf1ecc01db6de773bef3c782b3c292f8
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-23 16:00:08 +0900

    Homeにjsonを利用して繰り返し処理を採用

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+  import { reactive } from 'vue';
+  import sourceData from '@/data.json'
+  const destinations = reactive(sourceData.destinations)
+</script>
+
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="destination.slug"
+      >
+        <h2
+          v-text="destination.name"
+        ></h2>
+        <img
+          :src="`/images/${destination.image}`"
+          :alt="destination.name"
+        >
+      </router-link>
+    </div>
   </div>
 </template>
