1325d2c9d95717c83d53104b55ea4948c069bb0c
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-23 22:10:29 +0900

    destinationをsourceDataから取得する処理を追加。
    destinationId()内でparseInt()するときにNaNになるケースがあるが、その対応はまた今度する。
    route.params.id が 数値以外ならばNaNになる。

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import sourceData from '@/data.json'
+const route = useRoute()
+const destinationId = computed(()=>{
+  // FIXME:
+  // parseIntできない場合のケアが出来ていない。
+  // const result = route.params.id.test(/^\d+$/) とかでよさそう。
+  // result: false ならば、 404とかに飛ばす必要がありそう。
+  return parseInt(route.params.id as string)
+})
+const destination = computed(()=>{
+  return sourceData.destinations.find(destination => destination.id === destinationId.value )
+})
+</script>
+
 <template>
-  <h2>Hello Destination</h2>
+  <h1>{{ destination.value.name }}</h1>
+  <h2>Hello Destination {{ destinationId }}</h2>
 </template>
