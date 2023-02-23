3a9d4944b8aee69ea0d5764815417ab1a81d246c
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-23 15:10:37 +0900

    いくつかのコンポーネントを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Brazil from '@/views/Brazil.vue'
+import Hawaii from '@/views/Hawaii.vue'
+import Jamaica from '@/views/Jamaica.vue'
+import Panama from '@/views/Panama.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
   {path: '/brazil', name: 'brazil', component: Brazil},
+  {path: '/hawaii', name: 'Hawaii', component: Hawaii},
+  {path: '/jamaica', name: 'Jamaica', component: Jamaica},
+  {path: '/panama', name: 'Panama', component: Panama},
 ]
 
 const router = createRouter({
