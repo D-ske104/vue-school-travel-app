b1a99e2d0f1ef879e3d9e890cc58e48453af6f0e
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-23 15:04:27 +0900

    Brazilページを追加した。 任意のコンポーネントのルーティングができることになる。

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
 </script>
 
 <template>
-  <router-link to="/">Home</router-link> |
-  <router-link to="/about">About</router-link>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+  </div>
   <div class="container">
     <router-view></router-view>
   </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
+import Brazil from '@/views/Brazil.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
-  {path: '/about', name: 'About', component: About},
+  {path: '/brazil', name: 'brazil', component: Brazil},
 ]
 
 const router = createRouter({
