277c51622a44d48289da601907667ef691802b97
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-25 22:40:42 +0900

    props example newsletterPopup

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,9 @@ const routes = [
     path: '/destination/:id/:slug',
     name: 'destination.show',
     component: ()=>import('@/views/DestinationShow.vue'),
-    props: true,
+    props: {
+      newsletterPopup: false,
+    },
   },
 ]
 
