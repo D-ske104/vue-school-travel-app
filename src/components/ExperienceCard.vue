31661a6b0db8fa872ac1396f596bae2a42784aef
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-25 23:31:55 +0900

    ExperienceCard.vue作成。

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { Experience } from '@/interfaces/experience'
+
+interface Props {
+  experience: Experience
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="class">
+    <img :src="`/images/${props.experience.image}`" :alt="props.experience.name">
+    <span class="card__text">
+      {{ props.experience.name }}
+    </span>
+  </div>
+</template>
--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -2,6 +2,7 @@
 import { computed, defineProps } from 'vue';
 import { useRoute } from 'vue-router';
 import sourceData from '@/data.json'
+import ExperienceCard from '@/components/ExperienceCard.vue';
 const route = useRoute()
 
 interface Props {
@@ -14,17 +15,27 @@ const destination = computed(() => {
   const destination = sourceData.destinations.find(
     destination => destination.id === props.id
   )
-  if (destination)
+  if (destination === undefined) {
+    throw new Error("destination is undefined")
+  }
   return destination
 })
 </script>
 
 <template>
-  <section v-if="destination" class="destination">
+  <section class="destination">
     <h1>{{ destination.name }}</h1>
     <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name">
       <p>{{ destination.description }}</p>
     </div>
+  </section>
+  <section class="experiences">
+    <h2>Top Experiences in {{ destination.name }}</h2>
+    <ExperienceCard
+      v-for="experience in destination.experiences"
+      :experience="experience"
+      :key="experience.slug"
+    ></ExperienceCard>
   </section>
 </template>
