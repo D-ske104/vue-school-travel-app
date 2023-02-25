8d3ab3d9f288e37d969f48333110b58cc8d5bb9e
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-25 12:44:45 +0900

    初期値が不要になったので、型利用する元ファイルをクラスではなくてinterfaceにした。

---
+++ src/interfaces/destination.ts
@@ -0,0 +1,37 @@
+import { Experience, experience } from './experience'
+
+const name = "Japan"
+const slug = "japan"
+const image = "japan.jpg"
+const id = 0
+const description = "This is Japan."
+const experiences = [experience]
+
+const destination = {
+  name,
+  slug,
+  image,
+  id,
+  description,
+  experiences,
+}
+
+interface Destination {
+  name: string
+  slug: string
+  image: string
+  id: number
+  description: string
+  experiences: Experience[]
+}
+
+// class Destination {
+//   name: string = name
+//   slug: string = slug
+//   image: string = image
+//   id: number = id
+//   description: string = description
+//   experiences: Experience[] = experiences
+// }
+
+export {destination, Destination}
---
+++ src/interfaces/experience.ts
@@ -0,0 +1,27 @@
+const name = "Fujiyama"
+const slug = "fuji-yama"
+const image = "fujiyama.jpg"
+const description = "This is Fujiyama"
+
+const experience = {
+  name,
+  slug,
+  image,
+  description
+}
+
+interface Experience {
+  name: string
+  slug: string
+  image: string
+  description: string
+}
+
+// class Experience {
+//   name: string = name
+//   slug: string = slug
+//   image: string = image
+//   description: string = description
+// }
+
+export {experience, Experience}
