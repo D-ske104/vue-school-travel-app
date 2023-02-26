19886943aea1d94ababe67c0d2d9338f2357c161
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-26 12:44:59 +0900

    add Navigation Guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '@/views/Home.vue'
+import sourceData from '@/data.json'
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   {path: '/', name: 'Home', component: Home},
   {
     path: '/destination/:id/:slug',
     name: 'destination.show',
     component: ()=>import('@/views/DestinationShow.vue'),
     props: (route: {params: {id: string, slug?: string}}) => ({...route.params, id: parseInt(route.params.id)}), // FIXME
+    beforeEnter(to, _from){
+      const exists = sourceData.destinations.find(
+        destination => {
+          if (Array.isArray(to.params.id)) {
+            throw new Error("id is not Array.")
+          }
+          return destination.id === parseInt(to.params.id)
+        }
+      )
+      if (!exists) return {name: 'NotFound'}
+    },
     children: [
       {
         path: ':experienceSlug',
@@ -28,5 +40,17 @@ const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+// router.beforeEach(async(to, _from) =>{
+//   const exists = sourceData.destinations.find(
+//     destination => {
+//       if (Array.isArray(to.params.id)) {
+//         throw new Error("id is not Array.")
+//       }
+//       destination.id === parseInt(to.params.id)
+//     }
+//   )
+//   if (!exists) return {name: 'NotFound'}
+// })
 
 export default router
