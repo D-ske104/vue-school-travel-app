ef3ea8f283ad4e98aa31a5da54266c23302d189c
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-26 11:37:45 +0900

    change transition from slide to moveUp

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import TheNavigation from '@/components/TheNavigation.vue'
   <TheNavigation/>
   <div class="container">
     <router-view v-slot="{Component}">
-      <transition name="slide" mode="out-in">
+      <transition name="moveUp" mode="out-in">
         <component :is="Component" :key="$route.path"></component>
       </transition>
     </router-view>
@@ -14,13 +14,28 @@ import TheNavigation from '@/components/TheNavigation.vue'
 </template>
 
 <style lang="css">
-  .slide-enter-active,
-  .slide-leave-active{
-    transition: opacity 1s, transform 1s;
-  }
-  .slide-enter-from,
-  .slide-leave-to{
-    opacity:0;
-    transform: translateX(-30%);
-  }
+.moveUp-enter-active {
+  animation: fadeIn 1s ease-in;
+}
+@keyframes fadeIn {
+  0% { opacity: 0; }
+  50% { opacity: 0.5; }
+  100% { opacity: 1; }
+}
+.moveUp-leave-active {
+  animation: moveUp 0.3s ease-in;
+}
+@keyframes moveUp {
+  0% { transform: translateY(0); }
+  100% { transform: translateY(-400px); }
+}
+/* .slide-enter-active,
+.slide-leave-active{
+  transition: opacity 1s, transform 1s;
+}
+.slide-enter-from,
+.slide-leave-to{
+  opacity:0;
+  transform: translateX(-30%);
+} */
 </style>
