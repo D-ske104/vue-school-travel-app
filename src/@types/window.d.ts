3ff543154f0e1dfa75e9999bb08488c045829b1f
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-26 18:12:44 +0900

    ログアウトページでトップページに遷移させるメソッドを作成

--- src/@types/window.d.ts
+++ src/@types/window.d.ts
@@ -1,4 +1,4 @@
 // [TypeScript で window 直下にいろいろ生やしたりグローバル変数を定義する | DevelopersIO](https://dev.classmethod.jp/articles/typings-of-window-object/)
 interface Window {
-  user: string
+  user: string | undefined
 }
--- src/views/Protected.vue
+++ src/views/Protected.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const username = ref(window.user);
+const logout = () => {
+  window.user = undefined
+  router.push({name: 'Home'})
+}
+</script>
+
 <template>
   <div>
-    <h1>Protected</h1>
+    <h1>Greetings, {{ username }}</h1>
+    <button @click="logout">Logout</button>
   </div>
 </template>
