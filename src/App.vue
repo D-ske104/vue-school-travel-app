14586798dec14bb1ca9970be580eb3b57b1be25e
Daisuke Kusatake <dosaisen@gmail.com>
2023-02-26 11:40:57 +0900

    fixed transition

--- src/App.vue
+++ src/App.vue
@@ -22,28 +22,4 @@ import TheNavigation from '@/components/TheNavigation.vue'
 .fade-leave-to{
   opacity:0;
 }
-/* .moveUp-enter-active {
-  animation: fadeIn 1s ease-in;
-}
-@keyframes fadeIn {
-  0% { opacity: 0; }
-  50% { opacity: 0.5; }
-  100% { opacity: 1; }
-}
-.moveUp-leave-active {
-  animation: moveUp 0.3s ease-in;
-}
-@keyframes moveUp {
-  0% { transform: translateY(0); }
-  100% { transform: translateY(-400px); }
-} */
-/* .slide-enter-active,
-.slide-leave-active{
-  transition: opacity 1s, transform 1s;
-}
-.slide-enter-from,
-.slide-leave-to{
-  opacity:0;
-  transform: translateX(-30%);
-} */
 </style>
